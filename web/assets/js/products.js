255b9154d3db4e04c83b228cd983310595cac0de
Benjamin Benson <bensonb10@gmail.com>
2021-12-03 16:11:33 -0600

    cs

--- web/assets/js/products.js
+++ web/assets/js/products.js
@@ -1,7 +1,7 @@
 const collections = Array.from(document.getElementsByClassName("items"));
 collections.forEach(collection => {
     collection.onclick = function () {
-        console.log(this)
+        console.log(collection)
         window.location.assign("/product?type=" + this.getAttribute("name"));
     }
 })
