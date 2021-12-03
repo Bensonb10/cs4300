0b0806468f5f4e3d771eef9412e6385527eb2c37
Benjamin Benson <bensonb10@gmail.com>
2021-12-02 22:38:44 -0600

    minimize variable changes

--- web/views/product/product_card.php
+++ web/views/product/product_card.php
@@ -1,7 +1,7 @@
 <div class="items">
     <div class="card">
-        <img src="<?php echo $__src; ?>" alt="" class="card-img">
-        <h3 class="card-title"><?php echo $__desc; ?></h3>
-        <span><?php echo $__price; ?></span>
+        <img src="<?php echo $prod->src; ?>" alt="" class="card-img">
+        <h3 class="card-title"><?php echo $prod->desc; ?></h3>
+        <span><?php echo $prod->price; ?></span>
     </div>
 </div>
