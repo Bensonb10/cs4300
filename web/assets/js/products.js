a83fb59a7a4077f8d51b7613cece7472025348a0
Benjamin Benson <bensonb10@gmail.com>
2021-12-03 16:03:02 -0600

    tweaks

--- web/assets/js/products.js
+++ web/assets/js/products.js
@@ -1,6 +1,7 @@
 const collections = Array.from(document.getElementsByClassName("items"));
 collections.forEach(collection => {
     collection.onclick = () => {
-        window.location.assign("/product?type=" + collection.name.replace(/\s/g, ""));
+        console.log(collection)
+        // window.location.assign("/product?type=" + collection.name.replace(/\s/g, ""));
     }
 })
