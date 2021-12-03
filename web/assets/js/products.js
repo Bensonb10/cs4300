cb15976afb31cbf87587121528937385ace57345
Benjamin Benson <bensonb10@gmail.com>
2021-12-03 16:10:48 -0600

    use this

--- web/assets/js/products.js
+++ web/assets/js/products.js
@@ -1,7 +1,7 @@
 const collections = Array.from(document.getElementsByClassName("items"));
 collections.forEach(collection => {
     collection.onclick = function () {
-        console.log(this.name)
-        window.location.assign("/product?type=" + this.name);
+        console.log(this)
+        window.location.assign("/product?type=" + this.getAttribute("name"));
     }
 })
