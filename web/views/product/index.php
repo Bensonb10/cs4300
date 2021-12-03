aa9047d26f63953530aa92af0ab1c78e0509e73d
Benjamin Benson <bensonb10@gmail.com>
2021-12-03 15:48:30 -0600

    get_result

--- web/views/product/index.php
+++ web/views/product/index.php
@@ -17,7 +17,8 @@
     $sql = "SELECT p.Id,p.FriendlyName,p.ImgSrc,p.Price,c.Name as Collection FROM product p LEFT JOIN collection c on p.CollectonId = c.Id WHERE c.Name = ?;";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $_GET["type"]);
-    $resp = $stmt->execute();
+    $stmt->execute();
+    $resp = $stmt->get_result();
 
     foreach ($resp as $record) {
         $prod = new Product($record["ImgSrc"], $record["FriendlyName"], $record["Price"]);
