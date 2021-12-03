c432a42d100d3c81b7aa54a6fadd20bb38905f62
Benjamin Benson <bensonb10@gmail.com>
2021-12-02 22:18:59 -0600

    format

--- web/views/product/product_card.php
+++ web/views/product/product_card.php
@@ -1,5 +1,7 @@
-<div class="card">
-<img src="<?php echo $__src; ?>" alt="" class="card-img">
-<h3 class="card-title"><?php echo $__desc; ?></h3>
-<span><?php echo $__price; ?></span>
+<div class="items">
+    <div class="card">
+        <img src="<?php echo $__src; ?>" alt="" class="card-img">
+        <h3 class="card-title"><?php echo $__desc; ?></h3>
+        <span><?php echo $__price; ?></span>
+    </div>
 </div>
