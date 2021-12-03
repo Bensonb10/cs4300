c3ac857f83f057026574ebee1bdc666371406d6d
Benjamin Benson <bensonb10@gmail.com>
2021-12-03 16:12:40 -0600

    remove console.log

--- web/assets/js/products.js
+++ web/assets/js/products.js
@@ -1,7 +1,6 @@
 const collections = Array.from(document.getElementsByClassName("items"));
 collections.forEach(collection => {
     collection.onclick = function () {
-        console.log(collection)
         window.location.assign("/product?type=" + this.getAttribute("name"));
     }
 })
