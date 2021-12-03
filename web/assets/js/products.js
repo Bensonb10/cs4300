4d4dc781802e3ab905bc85fb2e7b361fb609b86b
Benjamin Benson <bensonb10@gmail.com>
2021-12-02 21:40:10 -0600

    maybe?

--- web/assets/js/products.js
+++ web/assets/js/products.js
@@ -1,12 +1,12 @@
 const PRODUCTS_DATA = [
-    new Card("assets/img/product1.jpg", "Excepteur sint occaecat"),
-    new Card("assets/img/product2.jpg", "Lorem ipsum dolor"),
-    new Card("assets/img/product3.jpg", "cupidatat non proident"),
-    new Card("assets/img/product4.jpg", "Duis aute irure"),
-    new Card("assets/img/product5.jpg", "magna aliqua"),
-    new Card("assets/img/product6.jpg", "Lorem occaecat"),
-    new Card("assets/img/product7.jpg", "Occaecat Duis"),
-    new Card("assets/img/product8.jpg", "illum qui dolorem eum fugiat")
+    new Card("/assets/img/product1.jpg", "Excepteur sint occaecat"),
+    new Card("/assets/img/product2.jpg", "Lorem ipsum dolor"),
+    new Card("/assets/img/product3.jpg", "cupidatat non proident"),
+    new Card("/assets/img/product4.jpg", "Duis aute irure"),
+    new Card("/assets/img/product5.jpg", "magna aliqua"),
+    new Card("/assets/img/product6.jpg", "Lorem occaecat"),
+    new Card("/assets/img/product7.jpg", "Occaecat Duis"),
+    new Card("/assets/img/product8.jpg", "illum qui dolorem eum fugiat")
 ];
 
 const productsSec = document.getElementById("products-sec");
