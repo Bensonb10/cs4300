2ad82188e96ea8cd3eced8833b9987d7b9362511
Benjamin Benson <bensonb10@gmail.com>
2021-12-03 16:04:48 -0600

    cleanup

--- web/assets/js/products.js
+++ web/assets/js/products.js
@@ -1,7 +1,8 @@
 const collections = Array.from(document.getElementsByClassName("items"));
 collections.forEach(collection => {
     collection.onclick = () => {
-        console.log(collection)
+        console.log(collection.name)
         // window.location.assign("/product?type=" + collection.name.replace(/\s/g, ""));
+        window.location.assign("/product?type=" + collection.name);
     }
 })
--- web/views/home/collection_card.php
+++ web/views/home/collection_card.php
@@ -1,4 +1,4 @@
-<div class="items" name="<?php echo $prod->friendlyName; ?>">
+<div class="items" name="<?php echo $prod->Name; ?>">
     <div class="card">
         <img src="<?php echo $prod->src; ?>" alt="" class="card-img">
         <h3 class="card-title"><?php echo $prod->friendlyName; ?></h3>
